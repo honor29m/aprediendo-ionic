1a121465169052f9f6de782a46226ffe85833dd2
hrodriguez-payall <84398072+hrodriguez-payall@users.noreply.github.com>
2022-06-13 11:10:08 -0400

    Seccion 10
    Servicio para traer las peliculas de estreno

---
+++ 05-peliculasApp/src/app/services/movies.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getFeature() {
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-01-31&api_key=2a1f74aec58b3f46117c2d9885e1e6c8&language=es&include_image_language=es`)
+  }
+}
